6f3bfef1b601da22cb32df379c0134fb661ded07
Trey Dockendorf <tdockendorf@osc.edu>
2026-06-22 13:29:45 -0400

    Add initial filters

---
+++ filters/backend_check.py
@@ -0,0 +1,162 @@
+from pydantic import BaseModel, Field
+from fastapi import Request, HTTPException, status
+from typing import Optional
+import asyncio
+import httpx
+import logging
+
+
+class Filter:
+    class Valves(BaseModel):
+        log_level: str = Field(default="INFO", description="Logging level")
+        wait_header: str = Field(
+            default="x-osc-wait",
+            description="The header that determines if waiting for model",
+        )
+        wait_duration: int = Field(
+            default=180, description="How long to wait for model to become available"
+        )
+        wait_users: list = Field(
+            default=["oscchat"], description="List of users to enforce waiting."
+        )
+        k8_namespace: str = Field(default="dynamo", description="Kubernetes namespace")
+        pushgateway_url: str = Field(
+            default="http://pushgateway.prometheus.svc:9091",
+            description="Push gateway URL",
+        )
+
+    def __init__(self):
+        self.valves = self.Valves()
+        self.logger = logging.getLogger("backend_check")
+
+    async def inlet(
+        self,
+        body: dict,
+        __user__: dict = None,
+        __metadata__: dict = None,
+        __request__: Request = None,
+    ) -> dict:
+        # Check if request is from WebUI
+        interface = __metadata__.get("interface") if __metadata__ else None
+
+        if interface == "open-webui":
+            # self.logger.info("Request from WebUI")
+            return body
+        # self.logger.info("Direct API request")
+
+        model_metadata = __metadata__.get("model", {})
+        idx = model_metadata.get("urlIdx", None)
+        if idx is not None and __request__ is not None:
+            backend_url = __request__.app.state.config.OPENAI_API_BASE_URLS[idx]
+            # self.logger.info(f"Backend URL: {backend_url}")
+        else:
+            self.logger.info(
+                f"Unable to determine model index. model-metadata={model_metadata}"
+            )
+        unavailable = HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="The AI backend is temporarily unavailable. Please try again later.",
+            headers={"Retry-After": "120"},
+        )
+        headers = {
+            "Content-Type": "application/json",
+        }
+        models = None
+        async with httpx.AsyncClient() as client:
+            r = await client.get(f"{backend_url}/models", headers=headers)
+            if not r.is_success:
+                raise unavailable
+            models = r.json()
+
+        # Exit early if backend models detected
+        if len(models.get("data", [])) > 0:
+            self.logger.info(
+                f"Models detected on backend, skipping scale up: backend={backend_url}"
+            )
+            return body
+
+        model = body.get("model", "unknown")
+        metric_model = model.replace("/", "-")
+        user_name = __user__.get("name", "unknown") if __user__ else "anonymous"
+        metric_data = f"""
+# HELP dynamo_pending_request Indicates a pending Dynamo request
+# TYPE dynamo_pending_request gauge
+dynamo_pending_request{{model="{metric_model}",namespace="{self.valves.k8_namespace}"}} 1
+"""
+        metrics_url = f"{self.valves.pushgateway_url}/metrics/job/{self.valves.k8_namespace}-{metric_model}"
+        metrics_header = {"Content-Type": "text/plain"}
+        self.logger.info(
+            f"Scale up request: user={user_name} model={metric_model} backend={backend_url}"
+        )
+        async with httpx.AsyncClient() as client:
+            r = await client.post(
+                metrics_url, content=metric_data, headers=metrics_header
+            )
+            self.logger.info(
+                f"Scale up request completed (metric): status={r.status_code} body={r.text}"
+            )
+        wait = __request__.headers.get(self.valves.wait_header, "false")
+        should_wait = False
+        if wait.lower() == "true":
+            should_wait = True
+            self.logger.info(
+                f"{self.valves.wait_header} header is true, wait: wait-header={wait}"
+            )
+        if user_name in self.valves.wait_users:
+            should_wait = True
+            self.logger.info(f"User {user_name} is a wait user, waiting")
+        if not should_wait:
+            self.logger.info(
+                f"{self.valves.wait_header} header is not true and not a wait user, skip wait, raise unavailable: wait-header={wait}"
+            )
+            raise unavailable
+
+        delay = 10
+        retries = self.valves.wait_duration // delay
+        for attempt in range(1, retries + 1):
+            self.logger.info(f"Attempt {attempt} of {retries}...")
+            wait_models = None
+            async with httpx.AsyncClient() as client:
+                r = await client.get(f"{backend_url}/models", headers=headers)
+                if not r.is_success:
+                    raise unavailable
+                wait_models = r.json()
+            if len(wait_models.get("data", [])) > 0:
+                self.logger.info(
+                    f"Models available, breaking from wait loop: backend={backend_url} models={wait_models}"
+                )
+                return body
+            # Non-blocking pause allows other tasks to run in the background
+            self.logger.info(f"Waiting {delay} seconds before retrying...")
+            await asyncio.sleep(delay)
+
+        self.logger.info(f"Model wait timed out: backend={backend_url}")
+        raise unavailable
+
+        # End logic, rest left in case becomes necessary in the future
+        # Send request to trigger scale up
+        self.logger.info(
+            f"Scale up request: user={user_name} model={model} backend={backend_url}"
+        )
+        payload = {
+            "model": model,
+            "messages": [{"role": "user", "content": "Scaling up, what is your name?"}],
+        }
+        # Run the query twice to ensure the metric gets triggered
+        async with httpx.AsyncClient() as client:
+            r = await client.post(f"{backend_url}/chat/completions", json=payload)
+            self.logger.info(
+                f"Scale up request completed (first): status={r.status_code} body={r.text}"
+            )
+            await asyncio.sleep(2)
+            r = await client.post(f"{backend_url}/chat/completions", json=payload)
+            self.logger.info(
+                f"Scale up request completed (second): status={r.status_code} body={r.text}"
+            )
+
+        # base_url = __request__.base_url
+        # path_name = __request__.url.path
+        # print(f"base_url={base_url} path_name={path_name}")
+
+        # return body
+
---
+++ filters/logging.py
@@ -0,0 +1,45 @@
+from pydantic import BaseModel, Field
+from fastapi import Request
+from typing import Optional
+import logging
+
+
+class Filter:
+    class Valves(BaseModel):
+        log_level: str = Field(default="INFO", description="Logging level")
+
+    def __init__(self):
+        self.valves = self.Valves()
+        self.logger = logging.getLogger("api_usage")
+
+    async def inlet(
+        self,
+        body: dict,
+        __user__: dict = None,
+        __metadata__: dict = None,
+        __request__: Request = None,
+    ) -> dict:
+        # Check if request is from WebUI
+        interface = __metadata__.get("interface") if __metadata__ else None
+        url_path = __request__.url.path if __request__ else "unknown"
+        chat_id = __metadata__.get("chat_id") if __metadata__ else None
+        user_name = __user__.get("name", "unknown") if __user__ else "anonymous"
+        model = body.get("model", "unknown")
+
+        model_metadata = __metadata__.get("model", {}) if __metadata__ else None
+        idx = model_metadata.get("urlIdx", None) if model_metadata else None
+        if idx is not None and __request__ is not None:
+            backend_url = __request__.app.state.config.OPENAI_API_BASE_URLS[idx]
+            # self.logger.info(f"Backend URL: {backend_url}")
+        else:
+            backend_url = "unknown"
+            self.logger.info(
+                f"Unable to determine model index. model-metadata={model_metadata}"
+            )
+
+        self.logger.info(
+            f"Request: user={user_name} path={url_path} model={model} backend={backend_url} chat_id={chat_id or 'none'}"
+        )
+
+        return body
+
